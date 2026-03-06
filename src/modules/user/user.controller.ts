33402e756b86e848df7e7ab5a942943d21738e2b
Alex <david_alejandrorizo@outlook.com>
2026-03-06 14:46:32 -0600

    implements auth decorator

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,21 +1,25 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Auth } from '../auth/decorators/auth.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Auth('admin')
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @Auth()
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
+  @Auth()
   @Get(':term')
   findOne(@Param('term') term: string) {
     return this.userService.findOne(term);
