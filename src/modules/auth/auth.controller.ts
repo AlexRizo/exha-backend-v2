de7c46873f2193d3511067ba3db433658fd4aba6
Alex <david_alejandrorizo@outlook.com>
2026-03-06 15:20:35 -0600

    cookie message

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,10 @@ import { Controller, Post, UseGuards, Res, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import type { Request, Response } from 'express';
+import { GetUser } from './decorators/get-user.decorator';
+import type { UserPayload } from './interfaces/jwt.interface';
+import { JwtRefreshAuthGuard } from './guards/jwt-refresh-auth.guard';
+import { Auth } from './decorators/auth.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +15,23 @@ export class AuthController {
   @Post('login')
   login(@Res({ passthrough: true }) res: Response, @Req() req: Request) {
     return this.authService.login(res, req);
+  }
+
+  @Auth()
+  @Post('logout')
+  logout(
+    @Res({ passthrough: true }) res: Response,
+    @GetUser('id') userId: string,
+  ) {
+    return this.authService.logout(res, userId);
+  }
+
+  @UseGuards(JwtRefreshAuthGuard)
+  @Post('refresh')
+  refresh(
+    @Res({ passthrough: true }) res: Response,
+    @GetUser() user: UserPayload,
+  ) {
+    return this.authService.refresh(res, user.id, user.refreshToken);
   }
 }
--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,19 +1,37 @@
-import { UnauthorizedException } from '@nestjs/common';
+import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthUser } from '../interfaces/jwt.interface';
+import { Request } from 'express';
+import { getCookie } from 'src/common/getCookie';
 
 export class JwtAuthGuard extends AuthGuard('jwt') {
   handleRequest<TUser = AuthUser>(
     err: any,
     user: TUser,
     info: Error & { message?: string },
+    context: ExecutionContext,
   ): TUser {
-    if (err) throw err;
+    const req = context.switchToHttp().getRequest<Request>();
 
-    if (!user) {
-      const message = info?.message ?? 'El token no es válido o ya expiró';
+    // 1. Verificamos si la cookie existe en la request
+    const hasToken = getCookie(req, 'access_token');
 
-      throw new UnauthorizedException({ message });
+    // 2. Si no hay cookie, lanzamos un mensaje personalizado
+    if (!hasToken) {
+      throw new UnauthorizedException({
+        statusCode: 401,
+        message: 'Acceso denegado',
+        error: 'Missing Cookie',
+      });
+    }
+
+    // 3. Si hay cookie pero el token es inválido (err o no user)
+    if (err || !user) {
+      throw new UnauthorizedException({
+        statusCode: 401,
+        message: 'Tu sesión ha expirado o el token es inválido.',
+        error: 'Invalid Token',
+      });
     }
 
     return user;
