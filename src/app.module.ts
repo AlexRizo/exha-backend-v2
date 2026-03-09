c32947c65b1ae59d110e5c05cf44e486b7b1ed2a
Alex <david_alejandrorizo@outlook.com>
2026-03-09 11:58:04 -0600

    date res created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { ExamModule } from './modules/exam/exam.module';
 import { TopicModule } from './modules/topic/topic.module';
+import { DateModule } from './modules/date/date.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule, ExamModule, TopicModule],
+  imports: [PrismaModule, AuthModule, UserModule, ExamModule, TopicModule, DateModule],
 })
 export class AppModule {}
