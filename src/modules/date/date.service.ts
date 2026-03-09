b4e557b118afa44193c08a8710c47d888b1718e8
Alex <david_alejandrorizo@outlook.com>
2026-03-09 12:12:19 -0600

    find by dates

--- src/modules/date/date.service.ts
+++ src/modules/date/date.service.ts
@@ -16,4 +16,57 @@ export class DateService {
 
     return date;
   }
+
+  async findAll() {
+    const dates = await this.prismaService.date.findMany();
+
+    if (!dates || !dates.length) {
+      throw new NotFoundException('No se encontraron aplicaciones');
+    }
+
+    return dates;
+  }
+
+  async findByDate(date: string) {
+    const start = new Date(date);
+    start.setUTCHours(0, 0, 0, 0);
+
+    const end = new Date(date);
+    end.setUTCHours(23, 59, 59, 999);
+
+    const dates = await this.prismaService.date.findMany({
+      where: {
+        scheduleAt: {
+          gte: start,
+          lte: end,
+        },
+      },
+    });
+
+    if (!dates || !dates.length) {
+      throw new NotFoundException('No se encontraron aplicaciones');
+    }
+
+    return dates;
+  }
+
+  async findByDateRange(dates: string[]) {
+    const findDates = await this.prismaService.date.findMany({
+      where: {
+        scheduleAt: {
+          gte: dates[0],
+          lte: dates[1],
+        },
+      },
+      orderBy: {
+        scheduleAt: 'asc',
+      },
+    });
+
+    if (!findDates || !findDates.length) {
+      throw new NotFoundException('No se encontraron aplicaciones');
+    }
+
+    return findDates;
+  }
 }
