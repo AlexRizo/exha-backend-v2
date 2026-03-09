dada1997eb71548bacd55492c5dc73fe0121dd84
Alex <david_alejandrorizo@outlook.com>
2026-03-09 11:18:03 -0600

    create exam done

--- src/modules/exam/exam.service.ts
+++ src/modules/exam/exam.service.ts
@@ -1,12 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateExamDto } from './dto/create-exam.dto';
+import { nanoid } from 'nanoid';
 
 @Injectable()
 export class ExamService {
   constructor(private readonly prisma: PrismaService) {}
 
-  createExam(createExamDto: CreateExamDto) {
-    return 'This action adds a new exam';
+  async createExam(createExamDto: CreateExamDto) {
+    const code = nanoid(6);
+    const examCode = `EXHA-${code}`;
+
+    const examExists = await this.prisma.exam.findUnique({
+      where: {
+        code: examCode,
+      },
+    });
+
+    if (examExists) {
+      throw new ConflictException('El examen ya existe');
+    }
+
+    const exam = await this.prisma.exam.create({
+      data: {
+        ...createExamDto,
+        code: examCode,
+      },
+    });
+
+    return exam;
   }
 }
