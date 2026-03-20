c438740a7e13423740934671902c369aead838fb
Alex <david_alejandrorizo@outlook.com>
2026-03-20 10:16:57 -0600

    apply auth decorator

--- src/modules/topic/exam-topic.controller.ts
+++ src/modules/topic/exam-topic.controller.ts
@@ -1,14 +1,14 @@
 import { Body, Controller, Param, ParseUUIDPipe, Post } from '@nestjs/common';
 import { TopicService } from './topic.service';
-import { AllowedRole } from '../auth/decorators/allowed-role.decorator';
 import { Role } from '@prisma/client';
 import { CreateTopicDto } from './dto/create-topic.dto';
+import { Auth } from '../auth/decorators/auth.decorator';
 
 @Controller('exam/:examId/topic')
 export class ExamTopicController {
   constructor(private readonly topicService: TopicService) {}
 
-  @AllowedRole(Role.admin)
+  @Auth(Role.admin)
   @Post()
   create(
     @Param('examId', ParseUUIDPipe) examId: string,
