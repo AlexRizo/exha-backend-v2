c18c3b931346f4068f8ec696a89cb2f3512423bc
Alex <david_alejandrorizo@outlook.com>
2026-03-20 10:12:37 -0600

    create topic from exam

--- src/modules/topic/exam-topic.controller.ts
+++ src/modules/topic/exam-topic.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, ParseUUIDPipe, Post } from '@nestjs/common';
 import { TopicService } from './topic.service';
+import { AllowedRole } from '../auth/decorators/allowed-role.decorator';
+import { Role } from '@prisma/client';
+import { CreateTopicDto } from './dto/create-topic.dto';
 
 @Controller('exam/:examId/topic')
 export class ExamTopicController {
   constructor(private readonly topicService: TopicService) {}
+
+  @AllowedRole(Role.admin)
+  @Post()
+  create(
+    @Param('examId', ParseUUIDPipe) examId: string,
+    @Body() createTopicDto: CreateTopicDto,
+  ) {
+    return this.topicService.create(examId, createTopicDto);
+  }
 }
--- src/modules/topic/topic.controller.ts
+++ src/modules/topic/topic.controller.ts
@@ -1,16 +1,6 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  ParseUUIDPipe,
-  Post,
-} from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { TopicService } from './topic.service';
 import { Auth } from '../auth/decorators/auth.decorator';
-import { CreateTopicDto } from './dto/create-topic.dto';
-import { AllowedRole } from '../auth/decorators/allowed-role.decorator';
-import { Role } from '@prisma/client';
 
 @Auth()
 @Controller('topic')
@@ -30,16 +20,5 @@ export class TopicController {
   @Get('exam/:examId')
   findManyByExam(@Param('examId', ParseUUIDPipe) examId: string) {
     return this.topicService.findManyByExam(examId);
-  }
-
-  // ? Cuando inicias el segmento de la url con '/';
-  // ? partes a partir de la raiz, y no de @Controller('segmento/...')
-  @AllowedRole(Role.admin)
-  @Post('/exam/:examId/topic')
-  create(
-    @Param('examId', ParseUUIDPipe) examId: string,
-    @Body() createTopicDto: CreateTopicDto,
-  ) {
-    return this.topicService.create(examId, createTopicDto);
   }
 }
